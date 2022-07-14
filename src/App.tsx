3bf6bd867cbd37afffb1689218a5f16d1578307c
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-07-13 21:38:21 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,70 @@
 import styles from './App.module.css'
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './assets/powered.png';
 
 
 function App() {
+
+  const [altura, setAltura] = useState<number>()
+  const [peso, setPeso] = useState<number>()
+
+  const handleCalcular =()=>{
+
+    if(altura && peso){
+
+    }else{
+      alert('Preencha todos os campos')
+    }
+
+  }
   return (
     <>
-    <h1>Um dia vai chegar o dia </h1>
+  <div className={styles.main}>
+    <header>
+      <div className={styles.headerContainer}>
+        <img src={logo} alt="logo" width={150}/>
+      </div>
+    </header>
+    <div className={styles.container}>
+
+      <div className={styles.leftSide}>
+        <h1>Calcule seu imc</h1>
+
+        <p>IMC sigla para indíce de massa corporal ,parâmetro
+          adotado pela organização Mundial de Saúde 
+          para calcular o peso ideal para cada pessoa 
+        </p>
+
+        <input
+        type="number"
+        placeholder="digite sua altura ex 1.5 (em metros)"
+        value={altura}
+        onChange={e => setAltura((parseFloat(e.target.value)))}
+        
+        />
+
+        <input
+        type="number"
+        placeholder='Digite seu peso ex 48.8 (em Kilos)'
+        value={peso}
+        onChange={e=>setPeso(parseFloat(e.target.value))}
+      
+        />
+
+        <button onClick={handleCalcular}>Calcular</button>
+
+        
+
+
+      </div>
+
+      <div className={styles.rigthSide}>
+        ... direita
+      </div>
+
+    </div>
+
+  </div>
     </>
   );
 }
