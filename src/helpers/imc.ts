d0cd8a64394ff190396b6ce170c4e3a3d6852467
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-07-14 21:59:04 -0300

    Update imc.ts

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -1,7 +1,7 @@
 export const levels = [
     {title:'Magreza',color:'#96a3ab',icon:'dowm',imc:[0,18.5]},
     {title:'Normal',color:'#0ead69',icon:'up',imc:[18.6,24.9]},
-    {},
+    {title:'sobrepeso',color:'#e2b039',icon:'dowm',imc:[30.1,99]},
     {}
 
 ]
