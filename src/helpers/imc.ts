a68d2071eebc27f693c3d339773d3c361ded2950
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-07-14 21:18:02 -0300

    Create imc.ts

---
+++ src/helpers/imc.ts
@@ -0,0 +1,11 @@
+export const levels = [
+    {title:'Magreza',color:'#96a3ab',icon:'dowm',imc:[0,18.5]},
+    {title:'Normal',color:'#0ead69',icon:'up',imc:[18.6,24.9]},
+    {},
+    {}
+
+]
+
+export const calculateImc =(heigth:number,weigth:number)=>{
+
+}
