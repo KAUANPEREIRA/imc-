c6fee93a2a25e11b70c23ce689c0ecf6b0d26b35
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-07-18 20:41:32 -0300

    Update imc.ts

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -1,11 +1,19 @@
-export const levels = [
+export type Level = {
+    title:string
+    color:string
+    icon:string
+    imc : number[]
+
+}
+export const levels:Level[] = [
     {title:'Magreza',color:'#96a3ab',icon:'dowm',imc:[0,18.5]},
     {title:'Normal',color:'#0ead69',icon:'up',imc:[18.6,24.9]},
     {title:'sobrepeso',color:'#e2b039',icon:'dowm',imc:[30.1,99]},
-    {}
+    
 
 ]
 
 export const calculateImc =(heigth:number,weigth:number)=>{
+    const imc =  weigth/ (heigth* heigth)
 
 }
